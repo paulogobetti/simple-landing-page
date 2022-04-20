2e72ce7afd92fcfa68fefbd0a672c5213596af73
Paulo Gobetti <paulogobetti@outlook.com.br>
2022-04-20 14:50:44 -0300

    New sections

--- index.php
+++ index.php
@@ -24,8 +24,8 @@
 <body>
 
     <header>
-        <div class="container align-center">
-            <div>
+        <div class="container">
+            <div class="col-align-center">
                 <a href="#"><img src="img/slp-logo-icon.svg" width="150" class="margin-please"></a>
             </div>
         </div>
@@ -56,6 +56,7 @@
                                 <h3>
                                     Enter your details<br>to speak with a consultant:
                                 </h3>
+                                <br>
                                 <fieldset>
                                     <label for="nome" id="label-inline">Name:</label>
                                     <input type="text" name="nome" id="nome">
@@ -88,26 +89,99 @@
             </div>
         </section>
 
-        <section class="outgrid-box-container"> <!-- LOREM IPSUM -->
-            <div class="container">
-                <div class="row">
-                    <div class="col outgrid-box d-inline-block"> <!-- A COL -->
-                        <img src="img/mockup.png" class="full-width">
+        <br><br><br><br><br><br>
+
+        <section style="background: #f1f1f1;">
+            <div class="flexb-container">
+                <div class="flexb-wrap">
+                    <div class="flex-col-5"> <!-- A COL -->
+                        <div class="col-align-center">
+                            <div class="outgrid-box">
+                                <img src="img/slp-mockup.png" class="img-fluid">
+                            </div>
+                        </div>
                     </div>
-                    <div class="col"> <!-- B COL -->
-                        <div>
-                            <h2>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit!
-                            </h2>
-                            <br>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
+                    <div class="flex-col-5"> <!-- B COL -->
+                        <div class="col-align-middle">
+                            <div class="">
+                                <h2>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit!
+                                </h2>
+                                <br>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+
+        <br><br><br><br><br><br>
+
+        <section style="background: #f1f1f1; padding-top: 70px;" class="">
+            <div class="align-center uppercase">
+                <h2>Where to start?</h2>
+            </div>
+            <div class="flexb-container" id="begin">
+                <div class="flexb-wrap">
+                    <div class="flex-col-3">
+                        <div class="soft-card">
+                            <img src="img/slp-step-1.png" class="outgrid-img align-img-center" width="120">
+                            <p class="outgrid-txt-repair align-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex-col-3">
+                        <div class="soft-card">
+                            <img src="img/slp-step-2.png" class="outgrid-img align-img-center" width="120">
+                            <p class="outgrid-txt-repair align-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex-col-3">
+                        <div class="soft-card">
+                            <img src="img/slp-step-3.png" class="outgrid-img align-img-center" width="120">
+                            <p class="outgrid-txt-repair align-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <button style="padding: 15px 25px;" class="uppercase">
+                            Fale com um consultor
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br><br>
+
+        <section class="padding-please"> <!-- LOREM 2 -->
+            <div class="flexb-container">
+                <div class="flexb-wrap">
+                    <div class="flex-col-5">
+                        <div class="col-align-middle">
+                            <h2 class="align-right">
+                                Benefícios de<br>ser Hiperador
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="flex-col-5">
+                        <p class="border-left">
+                            Ofereça aos seus clientes um sistema completo e homologado em todo o país, a implantação e o suporte ficam por conta da Hiper.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br><br>
+        
     </main>
 
 </body>
